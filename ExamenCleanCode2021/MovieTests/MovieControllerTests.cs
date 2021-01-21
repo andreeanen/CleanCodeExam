745f06f869c92683292a6e17176d39670b6391fa
andreeanen <andreeanen@yahoo.com>
2021-01-21 13:31:37 +0100

    Fix test for descending order

--- ExamenCleanCode2021/MovieTests/MovieControllerTests.cs
+++ ExamenCleanCode2021/MovieTests/MovieControllerTests.cs
@@ -11,26 +11,6 @@ namespace MovieTests
     {
         public IHttpClientFactory clientFactory;
 
-
-
-
-        //[TestMethod]
-        //public void GetMovieById_OrdersIsInstantiated_ReturnsOk()
-        //{
-        //    var controller = new MovieController(clientFactory);
-        //    var mockMovie = new Movie()
-        //    {
-        //        Id = "tt0060196",
-        //        Title = "The Good, the Bad and the Ugly",
-        //        Rated = "8,8"
-        //    };
-        //    string id = "tt0060196";
-        //    var actual = controller.GetMovieById(id);
-
-        //    Assert.IsInstanceOfType(actual, typeof(OkObjectResult));
-        //}
-
-
         [TestMethod]
         public void GetOrderedMovies_ReturnAListWithMoviesOrderAscendingAfterRating()
         {
@@ -48,22 +28,6 @@ namespace MovieTests
                     Title= "Indiana Jones and the Last Crusade",
                     Rated ="7,6"
                 }
-            };
-
-            var expected = new List<Movie>()
-            {
-                new Movie
-                {
-                    Id= "1",
-                    Title= "Indiana Jones and the Last Crusade",
-                    Rated ="7,6"
-                },
-                new Movie
-                {
-                     Id="0",
-                     Title ="The Good, the Bad and the Ugly",
-                     Rated="8,8"
-                },
             };
 
             string howToOrderMovies = "ascending";
@@ -115,33 +79,14 @@ namespace MovieTests
 
             string howToOrderMovies = "descending";
             var controller = new MovieController(clientFactory);
-            var actual = controller.OrderMoviesByRating(mockMovies, howToOrderMovies);
+            var movieList = controller.OrderMoviesByRating(mockMovies, howToOrderMovies) as List<Movie>;
 
+            var expectedMovie = mockMovies[0];
+            var actualMovie = movieList[0];
+
+            Assert.AreEqual(expectedMovie, actualMovie);
 
         }
-
-
-        //[TestMethod]
-        //public void GetMovieByIdReturnsRightMovie()
-        //{
-        //    var mockMovies = new List<Movie>();
-
-        //    var movie = new Movie
-        //    {
-        //        Id = "0",
-        //        Title = "The Good, the Bad and the Ugly",
-        //        Rated = "8,8"
-        //    };
-
-        //    mockMovies.Add(movie);
-
-        //    var controller = new MovieController(clientFactory);
-        //    var testId = "1";
-        //    var actualResult = controller.GetMovieById("tt0060196") as OkObjectResult;
-        //    var ingredients = actualResult.Value as List<Movie>;
-        //    //Assert.AreEqual(testId, actual.id);
-
-        //}
 
     }
 }
